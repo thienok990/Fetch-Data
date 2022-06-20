38c729c06a1db095112502dc84bbe935cd045eb4
thang-nhatrangart <64763913+thang-nhatrangart@users.noreply.github.com>
2022-06-20 11:02:42 +0700

    Initial commit

---
+++ src/Layout.tsx
@@ -0,0 +1,34 @@
+import { Layout, Menu } from "antd";
+import { Routes, Route, Link } from "react-router-dom";
+import CounterPage from './features/counter/CounterPage'
+import React from "react";
+
+const { Header, Content, Footer } = Layout;
+const App: React.FC = () => (
+  <Layout className="layout">
+    <Header>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["post"]}>
+        <Menu.Item key="posts">
+          <Link to="/">Posts</Link>
+        </Menu.Item>
+        <Menu.Item key="counter">
+          <Link to="/counter">Counter</Link>
+        </Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: "0 50px" }}>
+      <div className="site-layout-content">
+        <Routes>
+          <Route path="/" element={<h1>posts</h1>} />
+          <Route path="counter" element={<CounterPage/>} />
+        </Routes>
+      </div>
+    </Content>
+    <Footer style={{ textAlign: "center" }}>
+      Ant Design ©2018 Created by Ant UED
+    </Footer>
+  </Layout>
+);
+
+export default App;
