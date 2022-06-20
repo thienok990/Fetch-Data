e68db2e19ee74aceb1adb75b1198a9e3d14e7dd8
Hoang Truong <tvhoang91@gmail.com>
2022-06-20 12:04:38 +0700

    Fix antd menu seleced

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,5 @@
 import { Layout, Menu } from "antd";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
 import CounterPage from "./features/counter/CounterPage";
 import React from "react";
 
@@ -7,38 +7,42 @@ const { Header, Content, Footer } = Layout;
 
 const pages = [
   {
-    key: "posts",
+    key: "/",
     label: <Link to="/">Posts</Link>,
   },
   {
-    key: "counter",
+    key: "/counter",
     label: <Link to="/counter">Counter</Link>,
   },
 ] as any;
 
-const App: React.FC = () => (
-  <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={["posts"]}
-        items={pages}
-      />
-    </Header>
-    <Content style={{ padding: "0 50px" }}>
-      <div className="site-layout-content">
-        <Routes>
-          <Route path="/" element={<h1>posts</h1>} />
-          <Route path="counter" element={<CounterPage />} />
-        </Routes>
-      </div>
-    </Content>
-    <Footer style={{ textAlign: "center" }}>
-      Ant Design ©2018 Created by Ant UED
-    </Footer>
-  </Layout>
-);
+const App: React.FC = () => {
+  let location = useLocation();
+
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          selectedKeys={[location.pathname]}
+          items={pages}
+        />
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <div className="site-layout-content">
+          <Routes>
+            <Route path="/" element={<h1>posts</h1>} />
+            <Route path="/counter" element={<CounterPage />} />
+          </Routes>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
 
 export default App;
