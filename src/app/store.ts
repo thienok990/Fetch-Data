00a07e507ec1b6767dfe7dc72a2a07c0797e0a14
Thien Cao <luongthien.yaycommerce@gmail.com>
2022-06-21 09:42:44 +0700

    Add post slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-
+import postReducer from '../features/posts/PostsSlice'
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    post: postReducer
   },
 });
 
---
+++ src/features/posts/PostsSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+export interface PostsState {
+  posts: Post[];
+}
+const initialState: PostsState = {
+  posts: [
+    {
+      userId: 1,
+      id: 1,
+      title: "1",
+      body: "1",
+    },
+  ],
+};
+
+export const PostsSlice = createSlice({
+  name: "post",
+  initialState,
+  reducers: {},
+  extraReducers: {},
+});
+
+export default PostsSlice.reducer;
