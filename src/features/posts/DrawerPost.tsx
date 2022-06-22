4678086c46dcd87b496ba79d97b2922dd7417def
Thien Cao <luongthien.yaycommerce@gmail.com>
2022-06-22 10:33:12 +0700

    Change UI

--- src/features/posts/DrawerPost.tsx
+++ src/features/posts/DrawerPost.tsx
@@ -1,5 +1,6 @@
 import { Drawer, Button, Space } from "antd";
-
+import { UserOutlined } from "@ant-design/icons";
+import TextArea from "antd/lib/input/TextArea";
 export const DrawerPost = (props: any) => {
   const { visible, setVisible, selectedPost } = props;
 
@@ -10,7 +11,7 @@ export const DrawerPost = (props: any) => {
   return (
     <div>
       <Drawer
-        title={selectedPost.title}
+        title="Edit Post"
         placement="right"
         width={500}
         onClose={onClose}
@@ -21,12 +22,23 @@ export const DrawerPost = (props: any) => {
               Cancel
             </Button>
             <Button type="primary" onClick={onClose}>
-              Oke
+              Save
             </Button>
           </Space>
         }
       >
-        <h1>{selectedPost.body}</h1>
+        <h1>
+          <UserOutlined />
+          {selectedPost.userId}
+        </h1>
+        <h1>
+          Title:
+          <TextArea showCount style={{ height: 120 }} value={selectedPost.title} />
+        </h1>
+        <h1>
+          Body:
+          <TextArea showCount style={{ height: 240 }} value={selectedPost.body} />
+        </h1>
       </Drawer>
     </div>
   );
