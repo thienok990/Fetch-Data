72f73466c4384628b9a88801370a4336af5a6475
Thien Cao <luongthien.yaycommerce@gmail.com>
2022-06-22 16:50:35 +0700

    Put column out function PostPage

--- src/features/posts/PostsPage.tsx
+++ src/features/posts/PostsPage.tsx
@@ -3,35 +3,35 @@ import { fetchPosts } from "./PostsSlice";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { Table } from "antd";
 
+const columns = [
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "User Id",
+    dataIndex: "userId",
+    key: "userId",
+  },
+  {
+    title: "Title",
+    dataIndex: "title",
+    key: "title",
+  },
+  {
+    title: "Body",
+    dataIndex: "body",
+    key: "body",
+  },
+];
+
 function PostsPage() {
   const dispatch = useAppDispatch();
   const dataSource = useAppSelector((state) => state.post.posts);
   useEffect(() => {
     dispatch(fetchPosts());
   }, []);
-
-  const columns = [
-    {
-      title: "ID",
-      dataIndex: "id",
-      key: "id",
-    },
-    {
-      title: "User Id",
-      dataIndex: "userId",
-      key: "userId",
-    },
-    {
-      title: "Title",
-      dataIndex: "title",
-      key: "title",
-    },
-    {
-      title: "Body",
-      dataIndex: "body",
-      key: "body",
-    },
-  ];
 
   return (
     <div>
